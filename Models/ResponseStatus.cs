0e77638fc8c09f7b4d924522200787821fba15ed
Tien Le <work.lethanhtien@gmail.com>
2023-12-08 07:52:37 +0700

    feat: add login & workspace pages

---
+++ Models/ResponseStatus.cs
@@ -0,0 +1,16 @@
+﻿namespace POSWebsite.Models
+{
+    public class ResponseStatus
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; }
+        public string Data { get; set; }
+
+        public ResponseStatus(bool status, string message, string data = "")
+        {
+            Status = status;
+            Message = message;
+            Data = data;
+        }
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using POSWebsite.Models;
 
 namespace POSWebsite.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private ResponseStatus res;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+        }
+        public ResponseStatus GetResponseStatus()
+        {
+            return res;
         }
 
         public void OnGet()
