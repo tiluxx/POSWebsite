3b57bf91c6af512131a609ea2f1d1edb71fa31a9
Tien Le <work.lethanhtien@gmail.com>
2023-12-26 23:32:11 +0700

    feat: edit revenue statistics

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -53,12 +53,12 @@ namespace POSWebsite.Pages
                 if (account.Roles.Contains("Admin"))
                 {
                     await SaveLogin(account);
-                    return Redirect("/Auth/Index");
+                    return Redirect("/HeadQuarter/Index");
                 }
                 else
                 {
                     await SaveLogin(account);
-                    return Redirect("/Head-quarter/Index");
+                    return Redirect("/Auth/Index");
                 }
             }
 
